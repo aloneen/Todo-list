a6ca4093134786c783cc9dee591ebbefca6148b5
aloneen <seisenbek2005@mail.ru>
2025-08-17 14:16:03 +0500

    Loading and Saving data func added

--- TodoList/Model.swift
+++ TodoList/Model.swift
@@ -26,21 +26,50 @@ class Model {
     
     var items: [ToDoItem] = []
     
+    
+    
+    init() {
+        loadData()
+    }
+    
+    
+    
     func addItem(title: String) {
         items.append(ToDoItem(title: title, date: Date(), isCompleted: false))
+        saveData()
     }
     
     func deleteItem(at index: Int) {
         items.remove(at: index)
+        saveData()
     }
     
     func renameItem(at index: Int, newTitle: String) {
         items[index].title = newTitle
+        saveData()
     }
     
     
     func toggleCompletion(at index: Int) {
         items[index].isCompleted.toggle()
+        saveData()
+    }
+    
+    
+    let url = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask)[0].appending(path: "database.data")
+    
+    private func saveData() {
+        print(url)
+        let data = try? JSONEncoder().encode(items)
+        try? data?.write(to: url)
+    }
+    
+    
+    private func loadData() {
+        if let data = try? Data(contentsOf: url) {
+            items = (try? JSONDecoder().decode([ToDoItem].self, from: data) ) ?? []
+        }
+        
     }
     
     
